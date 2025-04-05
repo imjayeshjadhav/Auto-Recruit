cd95a80e1ac3af5b5cb46e22131bed7738f4b41d
imjayeshjadhav <14jayeshjadhav@gmail.com>
2025-04-06 01:34:31 +0530

    added interview ai

--- client/app/page.js
+++ client/app/page.js
@@ -12,7 +12,7 @@ import Footer from '@/components/Footer';
 
 const Page = () => {
   useEffect(() => {
-    AOS.init({ duration: 1000, once: true });
+    AOS.init({ duration: 1000, once: true});
   }, []);
 
   return (
